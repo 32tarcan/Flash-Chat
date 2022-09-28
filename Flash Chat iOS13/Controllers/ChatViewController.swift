68539e459bbea60aff3817c04de9751048ecf9bb
32tarcan <bahadir.tarcan41@gmail.com>
2022-09-28 21:24:57 +0300

    32

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -6,35 +6,72 @@ import FirebaseFirestore
 import FirebaseAuth
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    
+    var messages = [
+        
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up!"),
+        
+        
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
-
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+       
+        
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         
         let firebaseAuth = Auth.auth()
-    do {
-      try firebaseAuth.signOut()
+        do {
+            try firebaseAuth.signOut()
+            
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
         
-        navigationController?.popToRootViewController(animated: true)
         
-    } catch let signOutError as NSError {
-      print("Error signing out: %@", signOutError)
-    }
-      
-
     }
     @IBAction func sendPressed(_ sender: UIButton) {
         
     }
     
+}
 
+extension ChatViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+          as! MessageCell
+        cell.label?.text = messages[indexPath.row].body
+        
+        return cell
+    }
+    
+
+    
+    
+    
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Bahadır Tarcan on 27.09.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    
+    let sender: String
+    let body: String
+    
+}
