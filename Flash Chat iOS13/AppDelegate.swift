0101802349960b1cb414a2b756d5bccaf4b58542
32tarcan <bahadir.tarcan41@gmail.com>
2022-10-01 14:31:46 +0300

    tarcan

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 import FirebaseFirestore
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 
@@ -20,11 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         FirebaseApp.configure()
-        
         let db = Firestore.firestore()
-        
         print(db)
         
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
 
         return true
     }
--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,14 +13,7 @@ class ChatViewController: UIViewController {
     
     let db = Firestore.firestore()
     
-    var messages = [
-        
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "a@b.com", body: "Hello!"),
-        Message(sender: "1@2.com", body: "What's up!"),
-        
-        
-    ]
+    var messages: [Message] = []
     
     
     override func viewDidLoad() {
@@ -31,8 +24,43 @@ class ChatViewController: UIViewController {
         navigationItem.hidesBackButton = true
         
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
-       
         
+        loadMessages()
+        
+        
+    }
+    
+    func loadMessages() {
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapShot, error in
+            self.messages = []
+
+            if let e = error {
+                
+                print("There was a issue retrieving data from FireStore. \(e)")
+            }else {
+                if let snapShotDocuments = querySnapShot?.documents{
+                    for doc in snapShotDocuments {
+                        let data = doc.data()
+                        if let  messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                                
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -52,21 +80,28 @@ class ChatViewController: UIViewController {
     @IBAction func sendPressed(_ sender: UIButton) {
         
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
+            ]) { (error) in
                 if let e = error {
                     print("There were a issue saving data to FireStore, \(e)")
                 }else {
                     print("Succesfully saved data")
                 }
                 
+                DispatchQueue.main.async {
+                    self.messageTextfield.text = ""
+                }
             }
             
             
         }
-        }
-        
     }
     
+}
+
 
 
 extension ChatViewController: UITableViewDataSource {
@@ -78,16 +113,36 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let message = messages[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-          as! MessageCell
-        cell.label?.text = messages[indexPath.row].body
+        as! MessageCell
+        cell.label?.text = message.body
+        
+        //This is message from current user.
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+            
+        //This is a message from another sender.
+        }else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
+      
         
         return cell
     }
     
+    
+    
+    
+    
 
-    
-    
-    
 }
